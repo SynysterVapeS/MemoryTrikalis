700350d7dcf43abf4fc6a1ab3a73910b5c3404a3
DrPierogi <124446759+DrPierogi@users.noreply.github.com>
2023-02-09 06:28:15 +0200

    Update Memory.java

--- src/Memory.java
+++ src/Memory.java
@@ -1,13 +1,14 @@
+import javax.sound.sampled.*;
 import javax.swing.*;
 import java.awt.*;
-import javax.swing.JLabel;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Objects;
 
-import static java.nio.file.Files.exists;
+
 
 
 public class Memory extends JFrame implements ActionListener {
@@ -18,27 +19,34 @@ public class Memory extends JFrame implements ActionListener {
     int correct = 0; // counter of correct pairs (8max=win)
 
 
-
-
     JFrame frame = new JFrame();
     JPanel[] panels = new JPanel[rows * columns];
     JButton[] buttons = new JButton[rows * columns];
     ArrayList<ImageIcon> images = new ArrayList<ImageIcon>();
-    private ArrayList<ImageIcon> imagine = new ArrayList<ImageIcon>();       // used this as an image holder to compare the first 2 elements every time we open a 3rd image
+    File file;
+    Clip clip;
+    private ArrayList<ImageIcon> imageHolder = new ArrayList<ImageIcon>();
+    ArrayList<Integer> openCards = new ArrayList<Integer>();
 
-    ArrayList<Integer> openCards = new ArrayList<Integer>();                // counter of the cards player opens so that we know when they open a 3rd card every time
 
-
-    public Memory() {
+    public Memory() throws UnsupportedAudioFileException, IOException, LineUnavailableException {
         frame.setSize(1920, 1080);
         frame.setResizable(false);
+        frame.setTitle("MEMORY GAME");
+        frame.setIconImage(new ImageIcon("src//0.png").getImage());
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLayout(new GridLayout(rows, columns));
 
-        for (int i = 0; i < noOfImages; i++) {                                       // used noOfImages instead of panels.length
+        file = new File("src//karavi.wav");
+        AudioInputStream audioStream = AudioSystem.getAudioInputStream(file);
+        clip = AudioSystem.getClip();
+        clip.open(audioStream);
+        clip.start();
+
+        for (int i = 0; i < noOfImages; i++) {      // used noOfImages instead of panels.length
             ImageIcon image1 = new ImageIcon("src//" + (i + 1) + ".jpg");
-            ImageIcon image2 = image1;                                               // used this way so that same images are actually the same image to compare later
-            images.add(image1);                                                       // had to add "src//" and remove "image"
+            ImageIcon image2 = image1;
+            images.add(image1); // had to add "src//" and remove "image"
             images.add(image2);
         }
         Collections.shuffle(images);
@@ -56,7 +64,7 @@ public class Memory extends JFrame implements ActionListener {
             panels[i].setBackground(Color.BLACK);
             frame.add(panels[i]);
         }
-            frame.setVisible(true);
+        frame.setVisible(true);
 
     }
 
@@ -67,32 +75,56 @@ public class Memory extends JFrame implements ActionListener {
 
         for (int j = 0; j < panels.length; j++) {
             if (buttons[j] == button) {
-                buttons[j].setIcon(images.get(j));                                         // It works!
-                imagine.add(images.get(j));                                          // adding to the list of comparison
-                openCards.add(j);                                                    // we add the button position to replace image if not a pair
+                buttons[j].setIcon(images.get(j)); // It works!
+                imageHolder.add(images.get(j)); // adding to the list of comparison
+                openCards.add(j);  // we add the button position
 
             }
-            if(openCards.size()==3){                                                 //needs to happen on the 3rd card open,otherwise player never sees the 2nd card
-                if(imagine.get(1).equals(imagine.get(0))) {
-                    correct++;
-                }
-                else if(!imagine.get(0).equals(imagine.get(1))){
-                    wrong++;
-                    buttons[openCards.get(1)].setIcon(new ImageIcon("src//0.png"));         // replacing images again if not a pair
-                    buttons[openCards.get(0)].setIcon(new ImageIcon("src//0.png"));
-                }
-                openCards.remove(1);                                                        //clearing first 2 spots on the new Arrays so that we can start over
-                openCards.remove(0);
-                imagine.remove(1);
-                imagine.remove(0);
-                System.out.println("You found " + correct + " pairs!");                            // for us to know what's happening
-                System.out.println("You made " + wrong + " mistakes!");
+                if (openCards.size() == 3) {
+                    if (imageHolder.get(0).equals(imageHolder.get(1)) && openCards.get(0) != openCards.get(1) && openCards.get(0) != openCards.get(2) && openCards.get(1) != openCards.get(2)) {
+                        correct++;
+                        buttons[openCards.get(0)].setEnabled(false);        // sets buttons of correct pair unclickable
+                        buttons[openCards.get(1)].setEnabled(false);
+                    } else {
+                        wrong++;
+                        buttons[openCards.get(1)].setIcon(new ImageIcon("src//0.png"));
+                        buttons[openCards.get(0)].setIcon(new ImageIcon("src//0.png"));
+                        buttons[openCards.get(2)].setIcon(imageHolder.get(2));                  // keeps open last picked image no matter what
+                    }
+                    openCards.remove(1);
+                    openCards.remove(0);
+                    imageHolder.remove(1);
+                    imageHolder.remove(0);
+                    System.out.println("You found " + correct + " pairs!");
+                    System.out.println("You made " + wrong + " mistakes!");
 
-                /*if(wrong==5){                                                                  // thought about creating funny loserpage and winnerpage
-                    frame.dispose();
-                    LoserPage loserpage = new LoserPage();
-                }*/
+                    if(correct==7 && openCards.size()==1){
+                        clip.stop();
+                        frame.dispose();
+                        try {
+                            WinnerPage winnerPage = new WinnerPage();
+                        } catch (LineUnavailableException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (UnsupportedAudioFileException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+                    if(wrong==8){
+                        clip.stop();
+                        frame.dispose();
+                        try {
+                            LoserPage loserPage = new LoserPage();
+                        } catch (UnsupportedAudioFileException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (IOException ex) {
+                            throw new RuntimeException(ex);
+                        } catch (LineUnavailableException ex) {
+                            throw new RuntimeException(ex);
+                        }
+                    }
+
+                }
             }
         }
-    }
-}
